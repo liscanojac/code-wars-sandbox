115706cc863e3cac797e3137d1ca413e9a58fead
Juan Canelon <liscanojac@gmail.com>
2022-09-16 14:32:50 -0300

    feat: LastDigitOfLargeNumber kata completed

---
+++ lastDigitOfLastNumber.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/5511b2f550906349a70004e1
+
+function lastDigit(str1, str2) {
+
+  if (!Number(str2)) return 1;
+
+  const baseNumber = Number(str1.substring(str1.length - 1));
+
+  const sameLastDigitAsBaseNumber = [0, 1, 5, 6];
+
+  if (sameLastDigitAsBaseNumber.includes(baseNumber)) return baseNumber;
+
+  let powerNumber = (Number(str2.substring(str2.length - 2)) % 4);
+
+  if(!powerNumber) powerNumber = 4;
+
+  const result = (baseNumber ** powerNumber).toString()
+
+  return Number(result.substring(result.length - 1));
+}
+lastDigit('4', '4')
