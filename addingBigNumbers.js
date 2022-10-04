a2c47570bff3eca8e761af19e8cb21dec3bc0bcb
Juan Canelon <liscanojac@gmail.com>
2022-10-04 15:33:17 -0300

    refactor: if statement improved

--- addingBigNumbers.js
+++ addingBigNumbers.js
@@ -16,16 +16,12 @@ function add(a, b) {
   for (let i = a.length - 1; i >= 0; i--) {
     const sum = (Number(a[i]) + Number(b[i]) + remaining).toString();
 
-    if (i === 0) {
-      result = sum + result;
-      break;
-    }
-    if (sum.length > 1) {
-      result = sum.substring(1) + result;
-      remaining = Number(sum.substring(0, 1));
-    } else {
+    if (sum.length === 1 || i === 0) {
       result = sum + result;
       remaining = 0;
+    } else {
+      result = sum.substring(1) + result;
+      remaining = Number(sum.substring(0, 1));
     }
   }
   return result;
