8803d0913b350194acbcc7f69e46e94cbd18ed02
Juan Canelon <liscanojac@gmail.com>
2023-02-13 15:50:06 -0300

    refactor: adding stopping point for moveKnight

--- katas/shortestKinghtPath/shortestKnightPathRecursive.ts
+++ katas/shortestKinghtPath/shortestKnightPathRecursive.ts
@@ -7,15 +7,16 @@ export function knight(start: string, finish: string): number {
 
   let movements = [getStartMovement(start)];
 
-  function moveKnight(start: Movement, finish: string): number {
+  function moveKnight(start: Movement | undefined, finish: string): number {
 
+    if(!start) return 0;
     const movementsDone = getMovements(start)
     const finishFound = movementsDone.find(movement => movement.position === finish);
 
     if (finishFound) return finishFound.move;
 
     movements = movements.concat(movementsDone);
-    return moveKnight(movements.shift()!, finish);
+    return moveKnight(movements.shift(), finish);
   }
   return moveKnight(movements.shift()!, finish);
 }
