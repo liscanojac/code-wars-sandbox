289d1ad20e85e41ccb511c821bb5d81aa7a82458
Juan Canelon <liscanojac@gmail.com>
2023-02-13 15:43:09 -0300

    feat: shortestKnightPath recursive solution

---
+++ katas/shortestKinghtPath/shortestKnightPathRecursive.ts
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/549ee8b47111a81214000941
+
+import { Movement } from "../../interfaces/shortestKnightPath";
+import { getMovements, getStartMovement } from "./getMovements";
+
+export function knight(start: string, finish: string): number {
+
+  let movements = [getStartMovement(start)];
+
+  function moveKnight(start: Movement, finish: string): number {
+
+    const movementsDone = getMovements(start)
+    const finishFound = movementsDone.find(movement => movement.position === finish);
+
+    if (finishFound) return finishFound.move;
+
+    movements = movements.concat(movementsDone);
+    return moveKnight(movements.shift()!, finish);
+  }
+  return moveKnight(movements.shift()!, finish);
+}
