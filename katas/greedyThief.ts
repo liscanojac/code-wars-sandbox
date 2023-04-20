9ef7b83e343a30ebfd4642c85393ccf24bf3a166
Juan Canelon <liscanojac@gmail.com>
2023-04-20 16:39:18 -0300

    feat: greedyThief started

---
+++ katas/greedyThief.ts
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/58296c407da141e2c7000271
+
+interface StoreItem {
+  weight: number;
+  price: number;
+}
+
+export function greedyThief(items: Array<StoreItem>, maxWeight: number): Array<StoreItem> {
+  
+}
